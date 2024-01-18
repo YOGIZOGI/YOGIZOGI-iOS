489402a46f2f09f5d98ab6482fb05468a873f623
jeong-inn <jung1nn@naver.com>
2024-01-18 18:37:48 +0900

    [Feature]SocialLoginView

--- YOZO_iOS/AppDelegate/SceneDelegate.swift
+++ YOZO_iOS/AppDelegate/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
            guard let windowScene = (scene as? UIWindowScene) else { return }
            
            let window = UIWindow(windowScene: windowScene)
-           window.rootViewController = LoginViewController() // 시작시 띄울 ViewController
+           window.rootViewController = SocialLoginViewController() // 시작시 띄울 ViewController
            window.makeKeyAndVisible()
            self.window = window
        }
